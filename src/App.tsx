8d40734336ad54c5dc55cffe41505fa6e41082a1
Øyvind Follan <ofollan@gmail.com>
2020-11-06 18:02:20 +0100

    La til en knapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import './App.css';
-import firebase from 'firebase';
-import 'firebase/firestore';
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import "firebase/firestore";
+import FooComponent from "./FooComponent";
 
 function App() {
-    if(firebase.apps.length === 0){
-        firebase.initializeApp({
-            apiKey: "AIzaSyD8oWGuYEs8f7QIti6pAag5kuManznGZhk",
-            authDomain: "marvitclimbs.firebaseapp.com",
-            databaseURL: "https://marvitclimbs.firebaseio.com",
-            projectId: "marvitclimbs",
-            storageBucket: "marvitclimbs.appspot.com",
-            messagingSenderId: "327503153719",
-            appId: "1:327503153719:web:c5d8becfec99740036cdb2",
-            measurementId: "G-Y7DWQWF9DC"
-        });
-    }
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD8oWGuYEs8f7QIti6pAag5kuManznGZhk",
+      authDomain: "marvitclimbs.firebaseapp.com",
+      databaseURL: "https://marvitclimbs.firebaseio.com",
+      projectId: "marvitclimbs",
+      storageBucket: "marvitclimbs.appspot.com",
+      messagingSenderId: "327503153719",
+      appId: "1:327503153719:web:c5d8becfec99740036cdb2",
+      measurementId: "G-Y7DWQWF9DC",
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-          Funker github actions???
-      </header>
+      <FooComponent />
+      <header className="App-header">Funker github actions???</header>
     </div>
   );
 }
---
+++ src/FooComponent.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+import firebase from "firebase";
+
+const FooComponent: FunctionComponent = () => {
+  const db = firebase.firestore();
+  const addSomething = async () => {
+    await db.collection("Foo").add({
+      navn: "Bernt",
+      data: "Her er det både tall 123 og tekst {foo: 1}",
+    });
+  };
+
+  return <button onClick={async () => await addSomething}>Trykk på meg</button>;
+};
+
+export default FooComponent;
